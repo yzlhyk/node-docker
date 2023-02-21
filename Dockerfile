245cbde8889947195853ea1c1b22a8a282d1085d
Lincoln <lincolny@ngis.com.au>
2023-02-21 10:58:36 +0800

    add mocha

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
 # syntax=docker/dockerfile:1
+FROM node:18.14.1 as base
 
-FROM node:18.14.1
-ENV NODE_ENV=production
-WORKDIR /app
-COPY ["package.json", "package-lock.json*", "./"]
-RUN npm install --production
+WORKDIR /code
+
+COPY package.json package.json
+COPY package-lock.json package-lock.json
+
+FROM base as test
+RUN npm ci
+COPY . .
+CMD [ "npm", "run", "test" ]
+
+FROM base as prod
+RUN npm ci --production
 COPY . .
 CMD [ "node", "server.js" ]
+
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const ronin = require('ronin-server');
+const ronin = require('ronin-server')
 const database = require('ronin-database')
-const mocks = require('ronin-mocks');
+const mocks = require('ronin-mocks')
 
 async function main() {
+
   try {
-    await database.connect( process.env.CONNECTIONSTRING);
+    await database.connect(process.env.CONNECTIONSTRING)
 
     const server = ronin.server({
       port: process.env.SERVER_PORT
-    });
+    })
 
-    server.use('/', mocks.server(server.router()));
+    server.use( '/foo', (req, res) => {
+      return res.json({ "foo": "bar" })
+    })
 
-    const result = await server.start();
-    console.info(result);
+     server.use('/', mocks.server(server.Router()));
+
+    const result = await server.start()
+    console.info(result)
+
   } catch (error) {
     console.error(error)
   }
 }
 
-main();
+main()
